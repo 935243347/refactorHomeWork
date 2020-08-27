ffc73e82f2fc1328fb5fb7d4c751401c0c971b88
Max Miao <miaoma2@oocl.com>
2020-08-27 21:02:41 +0800

    [homework][max] test: test should_return_B_when_rating_given_voyage_zone_china_length_5_history_1_profit_1_zone_east-indies

--- test/rankTest.js
+++ test/rankTest.js
@@ -224,3 +224,18 @@ rankTest('should_return_B_when_rating_given_voyage_zone_abc_length_17_history_1_
   let result = rating(voyage, history);
   t.is("B", result);
 })
+
+rankTest('should_return_B_when_rating_given_voyage_zone_china_length_5_history_1_profit_1_zone_east-indies', t => {
+  const voyage = {
+    zone: 'china',
+    length: 15,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 1,
+    }
+  ];
+  let result = rating(voyage, history);
+  t.is("B", result);
+})
