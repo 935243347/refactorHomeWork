ae8e29fa280adfb808c1916f46185db75d048e02
Max Miao <miaoma2@oocl.com>
2020-08-27 20:46:50 +0800

    [homework][max] test: test should_return_B_when_rating_given_voyage_length_3_empty_history

--- test/rankTest.js
+++ test/rankTest.js
@@ -12,5 +12,13 @@ rankTest('should_return_B_when_rating_given_voyage_length_19_empty_history', t =
   t.is("B", result);
 })
 
-
+rankTest('should_return_B_when_rating_given_voyage_length_3_empty_history', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 3,
+  };
+  const history = [];
+  let result = rating(voyage, history);
+  t.is("B", result);
+})
 
