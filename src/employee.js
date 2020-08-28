309d7cb45da8238937912524b3f5a63435e29dd8
Max Miao <miaoma2@oocl.com>
2020-08-28 21:17:27 +0800

    [homework][max] refactor: extract method getEmployeeType

--- src/employee.js
+++ src/employee.js
@@ -6,13 +6,17 @@ class Employee {
   }
 
   validateType (type) {
-    if (![
+    if (this.getEmployeeType(type)) {
+      throw new Error(`Employee cannot be of type ${type}`);
+    }
+  }
+
+  getEmployeeType(type) {
+    return ![
       'engineer',
       'manager',
       'salesman',
-    ].includes(type)) {
-      throw new Error(`Employee cannot be of type ${type}`);
-    }
+    ].includes(type);
   }
 
   toString () {
