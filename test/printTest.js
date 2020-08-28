6ebb6445ad6458f9d0ec235a1b5bdfd293a53836
Max Miao <miaoma2@oocl.com>
2020-08-28 20:57:46 +0800

    [homework][max] fix-bug: should_return_localdate_when_printOwing_given_invoice_borderSpacing_2_content

--- test/printTest.js
+++ test/printTest.js
@@ -1,20 +1,20 @@
 import {printOwing} from "../src/print";
 
-const rankTest = require('ava');
+const printTest = require('ava');
 
-rankTest('should_return_localdate_when_printOwing_given_invoice_borderSpacing_2_content', t => {
+printTest('should_return_localdate_when_printOwing_given_invoice_borderSpacing_2_content', t => {
     const today = new Date();
     const dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
     const invoice = {
-        borderSpacing: ['china', 'west-indies'],
+        borderSpacing: [{amount: 'china'}, {amount: 'west-indies'}],
         dueDate: dueDate,
         customer: 'Max'
     };
     let result = printOwing (invoice);
-    t.is("*************************** Customer Owes ***************************name: Maxamount: NaNamount: " + dueDate.toLocaleDateString(), result);
+    t.is("*************************** Customer Owes ***************************name: Maxamount: 0chinawest-indiesamount: " + dueDate.toLocaleDateString(), result);
 })
 
-rankTest('should_return_localdate_when_printOwing_given_invoice_enpty_borderSpacing', t => {
+printTest('should_return_localdate_when_printOwing_given_invoice_enpty_borderSpacing', t => {
     const today = new Date();
     const dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
     const invoice = {
