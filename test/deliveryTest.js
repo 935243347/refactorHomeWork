812dc36844545b048dc00494e38a2bca60a9e03c
Max Miao <miaoma2@oocl.com>
2020-08-28 21:31:26 +0800

    [homework][max] test: test should_return_3_when_deliveryDate_given_true_NY

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -15,3 +15,17 @@ deliveryDateTest('should_return_2_when_deliveryDate_given_true_MA', t => {
     let result = deliveryDate (anOrder, isRush);
     t.is(2, result);
 })
+
+deliveryDateTest('should_return_3_when_deliveryDate_given_true_NY', t => {
+    let isRush = true;
+    let anOrder = {
+        deliveryState: 'NY',
+        placedOn: {
+            plusDays : function (data) {
+                return data;
+            }
+        }
+    }
+    let result = deliveryDate (anOrder, isRush);
+    t.is(3, result);
+})
